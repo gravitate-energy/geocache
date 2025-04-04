b69c76f05f6730425cef30d769e2b1a475bbee7b
Hunter Davis <hunter.davis@capspire.com>
2025-04-04 09:24:29 -0500

    Added logging, exposed port on docker file, required api key through req header

--- main.go
+++ main.go
@@ -6,47 +6,26 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
-	"github.com/joho/godotenv"
 	cache "github.com/victorspringer/http-cache"
 	"github.com/victorspringer/http-cache/adapter/memory"
 )
 
-// The google maps api endpoint url
-var googleMapsURL = "https://maps.googleapis.com"
-
-// The google maps api key
-var googleMapsAPIKey string
-var capacity int
-var lifetime time.Duration
+// Constants
+const GOOGLE_MAPS_URL = "https://maps.googleapis.com"
+const CACHE_LIFETIME = "720h"
+const CACHE_CAPACITY = 10_000_000
 
 // Programs main function
 func main() {
 
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal(err)
-		os.Exit(1)
-	}
-
-	if os.Getenv("GOOGLE_MAPS_API_KEY") != "" {
-		googleMapsAPIKey = os.Getenv("GOOGLE_MAPS_API_KEY")
-	}
-
-	if os.Getenv("CACHE_CAPACITY") != "" {
-		capacity, err = strconv.Atoi(os.Getenv("CACHE_CAPACITY"))
-	} else {
-		capacity = 10000000
-	}
-	log.Println("capacity: " + fmt.Sprint(capacity))
+	log.Println("capacity: " + fmt.Sprintf("%d requests", CACHE_CAPACITY))
 
 	memcached, err := memory.NewAdapter(
 		memory.AdapterWithAlgorithm(memory.LRU),
-		memory.AdapterWithCapacity(capacity),
+		memory.AdapterWithCapacity(CACHE_CAPACITY),
 	)
 
 	if err != nil {
@@ -54,10 +33,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	if os.Getenv("CACHE_LIFETIME") != "" {
-		lifetime, _ = time.ParseDuration(os.Getenv("CACHE_LIFETIME"))
+	lifetime, err := time.ParseDuration(CACHE_LIFETIME)
+
+	if err != nil {
+		fmt.Println("Failed to parse CACHE_LIFETIME: " + err.Error())
+		os.Exit(1)
 	}
-	log.Println("lifetime: " + fmt.Sprint(lifetime))
 
 	cacheClient, err := cache.NewClient(
 		cache.ClientWithAdapter(memcached),
@@ -73,8 +54,28 @@ func main() {
 	handler := http.HandlerFunc(query)
 
 	// Wrap the handler with CORS middleware
-	http.Handle("/", cacheClient.Middleware(corsMiddleware(handler)))
+	// http.Handle("/", cacheClient.Middleware(corsMiddleware(handler)))
+	http.Handle("/health", logger(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Server is healthy"))
+	})))
+
+	http.Handle("/", cacheClient.Middleware(logger(corsMiddleware(handler))))
 	http.ListenAndServe(":8080", nil)
+}
+
+func logger(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		ip := r.Header.Get("X-Forwarded-For")
+		if ip == "" {
+			ip = r.RemoteAddr
+		}
+
+		log.Printf("%s [%s] %s", ip, r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
 
 // CORS middleware to add CORS headers
@@ -82,7 +83,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Google-Maps-API-Key")
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
@@ -97,13 +98,16 @@ func corsMiddleware(next http.Handler) http.Handler {
 // Send a query against the google maps api
 func query(w http.ResponseWriter, r *http.Request) {
 
+	// get googleMapsAPIKey from request header
+	googleMapsAPIKey := r.Header.Get("x-google-maps-api-key")
+
 	ruri := r.URL.RequestURI()
 
 	if googleMapsAPIKey != "" && !strings.Contains(ruri, "key=") {
 		ruri += "&key=" + googleMapsAPIKey
 	}
 
-	resp, err := http.Get(googleMapsURL + ruri)
+	resp, err := http.Get(GOOGLE_MAPS_URL + ruri)
 
 	if err != nil {
 		log.Fatal(err)
@@ -121,6 +125,5 @@ func query(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("date", resp.Header.Get("date"))
 	w.Header().Add("expires", resp.Header.Get("expires"))
 	w.Header().Add("alt-svc", resp.Header.Get("alt-svc"))
-
 	w.Write(body)
 }
