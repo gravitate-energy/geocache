82a5ba138720b8f43f5802e37ce01f6e608c83c3
James Vogel <jim.m.vogel@gmail.com>
2025-04-30 21:39:51 -0500

    chore: remove deprecated ioutil package
    Updates import from "io/ioutil" to "io" for reading the HTTP response body.
    
    This change aligns with modern Go practices and removes the deprecated ioutil package.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -118,7 +118,7 @@ func query(w http.ResponseWriter, r *http.Request) {
 
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
 		log.Fatal(err)
